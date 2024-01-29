bb2a310727db9b98468c31e47463c785bb966881
Madeline Scyphers <madelinescyphers@gmail.com>
2024-01-29 18:16:27 -0500

    Shortened p1 with a hashmap

--- 2023/01/p1.rs
+++ 2023/01/p1.rs
@@ -6,43 +6,52 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-
-const NUM_WORDS: [&str; 10] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-const DIGITS: [i8; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+use std::collections::HashMap;
 
 
 fn main() {
+    let numbers = HashMap::from([
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
     if let Ok(lines) = read_lines("./data.txt") {
         // Consumes the iterator, returns an (Optional) String
         let mut sum = 0;
         for str in lines.flatten() {
 
-            let mut first_digit = char::default();
-            let mut last_digit = char::default();
+            let mut first_digit = &0;
+            let mut last_digit = &0;
             let mut first_digit_found = false;
 
-            // let mut last_digit_found = false;
-            for (i, c) in str.chars().enumerate() {
-                if c.is_digit(10) {
-                    if !first_digit_found {
-                        first_digit = c;
-                        first_digit_found = true;
-                    }
-                    last_digit = c;
-                }
-                else {
-                    for num in NUM_WORDS.iter() {
-                        let l = num.len();
-                        if i + l <= str.len() {
-                            let s = parse_substring(&str[i..i + l]);
-                            if s != -1 {
-                                if !first_digit_found {
-                                    first_digit = s.to_string().chars().next().unwrap();
-                                    first_digit_found = true;
-                                }
-                                last_digit = s.to_string().chars().next().unwrap();
-                                break
+            for (i, _c) in str.chars().enumerate() {
+
+                for l in 1..6 {
+                    if i + l <= str.len() {
+                        if numbers.contains_key(&str[i..i + l]) {
+                            let mtch = numbers.get(&str[i..i + l]).expect("Key Error");
+                            if !first_digit_found {
+                                first_digit = mtch;
+                                first_digit_found = true;
                             }
+                            last_digit = mtch;
                         }
                     }
                 }
@@ -55,18 +64,11 @@ fn main() {
     }
 }
 
+
 // The output is wrapped in a Result to allow matching on errors.
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
-}
-
-// parse substring to digit, return -1 if not found
-fn parse_substring(substring: &str) -> i8 {
-    if NUM_WORDS.contains(&substring) {
-        return DIGITS[NUM_WORDS.iter().position(|&r| r == substring).unwrap()];
-    }
-    return -1;
 }
