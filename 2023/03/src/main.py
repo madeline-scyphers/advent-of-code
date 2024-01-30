16f0258a8b3f699d07dd85e052b915415dbe38e9
Madeline Scyphers <madelinescyphers@gmail.com>
2024-01-30 16:56:02 -0500

    Move problem 3a to just problem 3 and combine 3a and 3b to one file

--- 2023/03/src/main.py
+++ 2023/03/src/main.py
@@ -1,4 +1,4 @@
-"""Problem 3A"""
+"""Problem 3"""
 from __future__ import annotations
 
 
@@ -7,11 +7,11 @@ import os
 
 
 def main():
-    assert process_data(read_file(Path(__file__).parent.parent / "test_data.txt")) == 4361
-    print(process_data(read_file(Path(__file__).parent.parent / "data.txt")))
+    assert problem_3a(read_file(Path(__file__).parent.parent / "test_data.txt")) == 4361
+    print(problem_3a(read_file(Path(__file__).parent.parent / "data.txt")))
 
 
-def process_data(data: list[list[str]]) -> int:
+def problem_3a(data: list[list[str]]) -> int:
     """
     The engine schematic (your puzzle input) consists of a visual representation of the engine.
     There are lots of numbers and symbols you don't really understand,
@@ -34,6 +34,48 @@ def process_data(data: list[list[str]]) -> int:
     In this schematic, two numbers are not part numbers because they are not adjacent
     to a symbol: 114 (top right) and 58 (middle right).
     Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
+    """
+    running_sum = 0
+    for row_idx, line in enumerate(data):
+        current_num = []
+        is_parta_num = False
+        for col_idx, char in enumerate(line):
+            if char.isdigit():
+                current_num.append(char)
+                if not is_parta_num:
+                    is_parta_num = check_if_is_part_num(data, row_idx, col_idx)
+            else:
+                if is_parta_num:
+                    running_sum += int("".join(current_num))
+                is_parta_num = False
+                current_num = []
+    return running_sum
+
+
+
+def problem_3b(data: list[list[str]]) -> int:
+    """
+    A gear is any * symbol that is adjacent to exactly two part numbers.
+    Its gear ratio is the result of multiplying those two numbers together.
+
+    Consider the same engine schematic again:
+
+    467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...$.*....
+    .664.598..
+
+    In this schematic, there are two gears. The first is in the top left;
+    it has part numbers 467 and 35, so its gear ratio is 16345.
+    The second gear is in the lower right; its gear ratio is 451490.
+    (The * adjacent to 617 is not a gear because it is only adjacent to one part number.)
+    Adding up all of the gear ratios produces 467835.
     """
     running_sum = 0
     for row_idx, line in enumerate(data):
